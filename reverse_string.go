18145ca2d94fd049bcea69a18fc22986cfdf7c7d
Lyly <khlbolatovna@gmail.com>
2022-05-18 20:23:42 +0600

    added code to reverse_string.go

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,12 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+	var result []rune
+	arr := []rune(input)
+
+	for i := len(arr) - 1; i >= 0; i-- {
+		result = append(result, arr[i])
+	}
+
+	return string(result)
 }
